1896724ed454fdcf9b8e97b10bf36fa126b763eb
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-17 20:31:48 -0300

    Quick adjusts in sales table

--- db/migrate/20210217230234_create_sales.rb
+++ db/migrate/20210217230234_create_sales.rb
@@ -2,9 +2,10 @@ class CreateSales < ActiveRecord::Migration[6.0]
   def change
     create_table :sales do |t|
       t.float :total
-      t.integer :parceling
+      t.integer :parceling, default: 1
       t.integer :tax
-      t.boolean :paid
+      t.boolean :paid, default: false
+      t.datetime :sale_date
 
       t.timestamps
     end
