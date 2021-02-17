fd3bd127611a46ea485b38f59c62d422cb5eb0d6
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-17 19:58:55 -0300

    Generate product_client model

---
+++ app/models/product_client.rb
@@ -0,0 +1,4 @@
+class ProductClient < ApplicationRecord
+  belongs_to :product_id
+  belongs_to :client_id
+end
---
+++ db/migrate/20210217225832_create_product_clients.rb
@@ -0,0 +1,11 @@
+class CreateProductClients < ActiveRecord::Migration[6.0]
+  def change
+    create_table :product_clients do |t|
+      t.references :product_id, null: false, foreign_key: true
+      t.references :client_id, null: false, foreign_key: true
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
