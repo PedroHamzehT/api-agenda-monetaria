0e162bfab5d362b39bb21e0e39ee05d58ac29fd5
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-17 22:59:26 -0300

    Create association between client and product through product_client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ApplicationRecord
   validates_presence_of :name
 
-  has_many :products, through: :product_clients
+  has_many :products, through: :product_client
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   validates_presence_of :name, :value
+
+  belongs_to :client, through: :product_client
 end
