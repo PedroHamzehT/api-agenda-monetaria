af618a1a361dba28cf4e71929c24a0a5fd2bdd5d
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-18 20:17:38 -0300

    Create the spec structure

--- spec/models/sale_spec.rb
+++ spec/models/sale_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Sale, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'valid values' do
+    it 'should create sale' do
+      create(:sale)
+
+      expect(Sale.all.size).to eq(1)
+    end
+  end
+
+  context 'invalid values' do
+    it 'sale_date blank should not be valid' do
+      sale = build(:sale, sale_date: nil)
+
+      expect(sale).to_not be_valid
+    end
+  end
+
+  it 'total should be calculated by products values and quantity'
+
+  it 'when parcelling is greater than 1 should calculate the values'
+
+  it 'when there is a tax should calculate the values'
+
+  it 'when history_payment sum be equal the sale total, sale should be paid'
 end
