7564c125e3f705fc193fcf891e1586f4596c8552
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-17 22:48:32 -0300

    Create validation to Client name

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'valid values' do
+    it 'should create client' do
+      create(:client)
+
+      expect(Client.all.size).to eq(1)
+    end
+  end
+
+  context 'invalid values' do
+    it 'name blank should not be valid' do
+      client = build(:client, name: '')
+
+      expect(client).to_not be_valid
+    end
+  end
 end
