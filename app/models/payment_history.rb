36e0a3ffe23c9d74a96088da156f3ba1bfa55420
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-19 00:27:00 -0300

    Get all sale it cases tests pass

--- app/models/payment_history.rb
+++ app/models/payment_history.rb
@@ -1,6 +1,14 @@
 class PaymentHistory < ApplicationRecord
+  after_save :update_sale_status
+
   validates_presence_of :date
   validates :pay_value, numericality: { greater_than: 0 }
 
   belongs_to :sale
+
+  private
+
+  def update_sale_status
+    sale.update_paid
+  end
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,10 +1,21 @@
 class Sale < ApplicationRecord
+  validates_presence_of :sale_date
+
   has_many :sale_products
   has_many :products, through: :sale_products
+  has_many :payment_histories
 
   def update_total
     self.total = sale_products.map(&:total).sum
 
     self.total = ((total / parcelling) * (1 + tax.to_f / 100)) * parcelling if tax.positive?
+
+    save!
+  end
+
+  def update_paid
+    self.paid = true if payment_histories.map(&:pay_value).sum >= total
+
+    save!
   end
 end
