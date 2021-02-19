892a7c9f6ed7acc4b2853fe2a074adc89c997df6
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-18 23:53:49 -0300

    Fix the has_many association of product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   validates_presence_of :name
   validates :value, numericality: { greater_than: 0 }
 
-  has_many :clients, through: :product_client
+  has_many :sales, through: :sale_products
 end
