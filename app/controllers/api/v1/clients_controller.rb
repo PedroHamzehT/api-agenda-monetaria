7a3d1f363abb1924136a6212b20540d54744f907
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-23 13:51:52 -0300

    Create the action create for clients

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -7,9 +7,26 @@ module Api
       def index
         @clients = Client.order('name')
 
-        render json: @clients, status: 201
+        render json: @clients, status: 200
       rescue StandardError => e
         render json: { error: e.message }, status: 500
+      end
+
+      def create
+        @client = Client.new(client_params)
+        if @client.save
+          render json: @client, status: 201
+        else
+          render json: { error: @client.errors.full_messages }, status: 400
+        end
+      rescue StandardError => e
+        render json: { error: e.message }, status: 500
+      end
+
+      private
+
+      def client_params
+        params.require(:client).permit(:name, :email, :cellphone, :description)
       end
     end
   end
