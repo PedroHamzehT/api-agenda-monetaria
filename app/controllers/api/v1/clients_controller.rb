88209a43150f0dbacbaff13b11fcd20671366278
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-23 09:50:53 -0300

    Add some stuffs recommended by rubocop

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 module Api
   module V1
+    # Responsible for the clients api endpoints
     class ClientsController < ApplicationController
       def index
         @clients = Client.order('name')
