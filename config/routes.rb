c47c882d7815be82b1cf4d74b5abae65d1ff9bbf
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-23 09:41:55 -0300

    Create clients routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :clients, only: %i[index create update]
+      get '/clients/:id/sales', to: 'clients#sales', as: 'client_sales'
+    end
+  end
 end
