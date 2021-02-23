7a7ca957efdbeffdc18809d044b99a9a58fdb39f
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-23 09:45:01 -0300

    Create the action index for clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,9 @@
 class ClientsController < ApplicationController
+  def index
+    @clients = Client.order('name')
+
+    render json: @clients, status: 201
+  rescue StandardError => e
+    render json: { error: e.message }, status: 500
+  end
 end
