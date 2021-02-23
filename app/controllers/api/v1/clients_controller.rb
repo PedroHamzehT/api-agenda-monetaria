9a55621724f65ed449ceadaa5e185831ae849764
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-23 13:58:57 -0300

    Create the update action for clients

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -4,6 +4,8 @@ module Api
   module V1
     # Responsible for the clients api endpoints
     class ClientsController < ApplicationController
+      before_action :set_client, only: %i[update]
+
       def index
         @clients = Client.order('name')
 
@@ -23,10 +25,26 @@ module Api
         render json: { error: e.message }, status: 500
       end
 
+      def update
+        if @client.update(client_params)
+          render json: @client, status: 200
+        else
+          render json: { error: @client.errors.full_messages }, status: 400
+        end
+      rescue StandardError => e
+        render json: { error: e.message }, status: 500
+      end
+
       private
 
       def client_params
         params.require(:client).permit(:name, :email, :cellphone, :description)
+      end
+
+      def set_client
+        @client = Client.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: 'Client not found' }
       end
     end
   end
