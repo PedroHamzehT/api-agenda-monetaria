cd48be872385354d56514cc81c8256538b01f6c9
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-23 22:06:45 -0300

    Create the action update for sales

--- app/controllers/api/v1/sales_controller.rb
+++ app/controllers/api/v1/sales_controller.rb
@@ -4,6 +4,8 @@ module Api
   module V1
     # Responsible for the sales api endpoints
     class SalesController < ApplicationController
+      before_action :set_sale, only: %i[update]
+
       def index
         @sales = Sale.order('sale_date')
         @sales = @sales.where(client_id: params[:client]) if params[:client].present?
@@ -24,10 +26,26 @@ module Api
         render json: { error: e.message }, status: 500
       end
 
+      def update
+        if @sale.update(sale_params)
+          render json: @sale, status: 200
+        else
+          render json: { error: @sale.errors.full_message }, status: 400
+        end
+      rescue StandardError => e
+        render json: { error: e.message }, status: 500
+      end
+
       private
 
       def sale_params
         params.require(:sale).permit(:sale_date, :parcelling, :tax, :client_id)
+      end
+
+      def set_sale
+        @sale = Sale.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: 'Sale not found' }
       end
     end
   end
