901b1a139ae311e30c73d21ebc11ed84076ad52e
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-23 21:44:00 -0300

    Create the action index for sales

--- app/controllers/api/v1/sales_controller.rb
+++ app/controllers/api/v1/sales_controller.rb
@@ -4,6 +4,13 @@ module Api
   module V1
     # Responsible for the sales api endpoints
     class SalesController < ApplicationController
+      def index
+        @sales = Sale.order('sale_date')
+
+        render json: @sales, status: 200
+      rescue StandardError => e
+        render json: { error: e.message }, status: 500
+      end
     end
   end
 end
