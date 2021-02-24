6f7e33d39fc4a0874c1da1a2e3d05f3716ec982e
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-23 21:38:59 -0300

    Create the action update for products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,6 +4,8 @@ module Api
   module V1
     # Responsible for the products api endpoints
     class ProductsController < ApplicationController
+      before_action :set_product, only: %i[update]
+
       def index
         @products = Product.order(:name)
 
@@ -17,10 +19,32 @@ module Api
         if @product.save
           render json: @product, status: 201
         else
-          render json: { error: @product.errors.full_message }
+          render json: { error: @product.errors.full_message }, status: 400
         end
       rescue StandardError => e
         render json: { error: e.message }, status: 500
+      end
+
+      def update
+        if @product.update(product_params)
+          render json: @product, status: 200
+        else
+          render json: { error: @product.errors.full_message }, status: 400
+        end
+      rescue StandardError => e
+        render json: { error: e.message }, status: 500
+      end
+
+      private
+
+      def product_params
+        params.require(:product).permit(:name, :value, :description)
+      end
+
+      def set_product
+        @product = Product.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: 'Product not found' }, status: 400
       end
     end
   end
