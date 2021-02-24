9c897aad789697ccf3ef7b8f271b6c5f2770ae26
PedroHamzehT <pedrohamzeh@gmail.com>
2021-02-23 22:21:39 -0300

    Create the action update for payment histories

--- app/controllers/api/v1/payment_histories_controller.rb
+++ app/controllers/api/v1/payment_histories_controller.rb
@@ -4,6 +4,8 @@ module Api
   module V1
     # Responsible for the payment histories api endpoints
     class PaymentHistoriesController < ApplicationController
+      before_action :set_payment, only: %i[update]
+
       def create
         @payment = PaymentHistory.new(payment_params)
         if @payment.save
@@ -15,10 +17,26 @@ module Api
         render json: { error: e.message }, status: 500
       end
 
+      def update
+        if @payment.update(payment_params)
+          render json: @payment, status: 200
+        else
+          render json: { error: @payment.errors.full_message }, status: 400
+        end
+      rescue StandardError => e
+        render json: { error: e.message }, status: 500
+      end
+
       private
 
       def payment_params
         params.require(:payment_history).permit(:date, :pay_value, :sale_id)
+      end
+
+      def set_payment
+        @payment = PaymentHistory.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: 'Payment history not found' }, status: 400
       end
     end
   end
