06aea7617ef4c55ce00cd62990c28bcbcc542620
PedroHamzehT <pedrohamzeh@gmail.com>
2021-03-05 17:18:44 -0300

    Improve payment_history factory changing the pay_value genarator to Faker::Number.between

--- spec/factories/payment_histories.rb
+++ spec/factories/payment_histories.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :payment_history do
-    pay_value { Faker::Number.decimal(l_digits: 2) }
+    pay_value { Faker::Number.between(from: 100.0, to: 2000.00).round(2) }
     date { DateTime.now }
     sale_id { create(:sale).id }
   end
