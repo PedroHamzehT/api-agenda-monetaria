34d9f6a41613b7d8c57e81dd544e20fa2cc8891d
PedroHamzehT <pedrohamzeh@gmail.com>
2021-03-09 13:49:51 -0300

    Improve the products factory to never generate a value 0

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :product do
     name { Faker::Food.fruits }
-    value { Faker::Number.decimal(l_digits: 2) }
+    value { Faker::Number.between(from: 100.0, to: 1000.00).round(2) }
     description { Faker::Food.description }
   end
 end
