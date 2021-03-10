b25496f04e3f1553b2cda02a4969b610987251a4
PedroHamzehT <pedrohamzeh@gmail.com>
2021-03-10 09:25:32 -0300

    Create the user model tests structure

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'valid values' do
+    it 'should create the user'
+
+    it 'the user password should be encrypted'
+  end
+
+  context 'invalid values' do
+    it 'should not create the user when email is missing'
+
+    it 'should not create the user when name is missing'
+
+    it 'should not create the user when email already exists'
+
+    it 'should not create the user when password is missing'
+  end
 end
