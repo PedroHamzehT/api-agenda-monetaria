4d54713197c9040b354ff97d21a3f4fbea2b4a9c
PedroHamzehT <pedrohamzeh@gmail.com>
2021-03-13 09:57:16 -0300

    Improve users requests tests outline

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -3,12 +3,16 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   describe 'POST /api/v1/sign_up' do
     context 'valid parameters' do
+      it 'should return success status'
+
       it 'should create a user'
 
       it 'should return a token'
     end
 
     context 'invalid parameters' do
+      it 'should return bad request status'
+
       it 'should warn when email is missing'
 
       it 'should warn when name is missing'
@@ -21,10 +25,14 @@ RSpec.describe "Users", type: :request do
 
   describe 'POST /api/v1/sign_in' do
     context 'valid parameters' do
+      it 'should return success status'
+
       it 'should find the user and return a token'
     end
 
     context 'inavlid parameters' do
+      it 'should return bad request status'
+
       it 'should warn when password is missing'
 
       it 'should warn when email already exists'
@@ -32,6 +40,18 @@ RSpec.describe "Users", type: :request do
       it 'should warn when email not found'
 
       it 'should warn when password not found with the email'
+    end
+  end
+
+  describe 'PUT /api/v1/user' do
+    context 'valid parameters' do
+      it 'should return success status'
+
+      it 'should update the user'
+    end
+
+    context 'invalid parameters' do
+      it 'should return bad request status'
     end
   end
 end
