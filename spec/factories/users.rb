002dfee2a968489aea09a44d597153fd8cdde978
PedroHamzehT <pedrohamzeh@gmail.com>
2021-03-18 20:10:32 -0300

    Improve users factory to use Faker email and Faker name

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name { 'User' }
-    email { 'user@example.com' }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
   end
