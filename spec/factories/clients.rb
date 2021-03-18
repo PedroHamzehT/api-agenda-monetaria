3a88bcbef35696c17a3e914ef7e9cd52f6e9c5c6
PedroHamzehT <pedrohamzeh@gmail.com>
2021-03-18 20:08:13 -0300

    Fix clients factory when is calling the create(:user)

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name { Faker::Name.name }
     email { Faker::Internet.email }
     description { 'Some description' }
-    user_id { create(user).id }
+    user_id { create(:user).id }
   end
 end
