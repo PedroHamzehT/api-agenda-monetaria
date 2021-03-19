f441c8a39e66d52eab7fff723d07e43dddbd1e05
PedroHamzehT <pedrohamzeh@gmail.com>
2021-03-18 21:55:37 -0300

    Add the user_id for products factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     name { Faker::Food.fruits }
     value { Faker::Number.between(from: 100.0, to: 1000.00).round(2) }
     description { Faker::Food.description }
+    user_id { create(:user).id }
   end
 end
